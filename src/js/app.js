94156753c192171b69eb8c3624688800b68cce39
AlexMyz <60917617+AlexMyz@users.noreply.github.com>
2023-04-05 13:57:34 +0000

    adaptive

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
 import * as flsFunctions from "./modules/function.js";
+import burger from './modules/burger.js'
 
+burger()
 flsFunctions.isWebp()
---
+++ src/js/modules/burger.js
@@ -0,0 +1,9 @@
+const burger = () => {
+  document.querySelector('.menu__burger').addEventListener('click', function() {
+    this.classList.toggle('_active')
+    document.querySelector('.menu__nav').classList.toggle('_active')
+    document.querySelector('body').classList.toggle('_lock')
+  })
+}
+
+export default burger
